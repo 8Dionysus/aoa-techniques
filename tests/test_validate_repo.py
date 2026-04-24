581343d358a387177cd2e979a571d48984d87de4
8Dionysus <gerhmangrant@gmail.com>
2026-04-23 18:40:27 -0600

    [codex] Prepare v0.4.2 release

--- tests/test_validate_repo.py
+++ tests/test_validate_repo.py
@@ -1422,6 +1422,7 @@ class TechniqueContentSmokeTests(unittest.TestCase):
         self.assertEqual(
             (
                 "[Unreleased]",
+                "[0.4.2] - 2026-04-23",
                 "[0.4.1] - 2026-04-19",
                 "[0.4.0] - 2026-04-10",
                 "[0.3.0] - 2026-04-01",
