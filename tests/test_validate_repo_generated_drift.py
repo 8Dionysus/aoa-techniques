1ab377a9ddc2dcfc35fa4f4b3db8fd77b0e69d0c
Dionysus <gerhmangrant@gmail.com>
2026-08-22 22:13:43 -0600

    docs: prepare aoa-techniques v0.6.0 release (#500)
    * docs: prepare aoa-techniques v0.6.0 release
    
    * build: refresh portable KAG family for v0.6.0

--- tests/test_validate_repo_generated_drift.py
+++ tests/test_validate_repo_generated_drift.py
@@ -509,6 +509,7 @@ class ValidateRepoGeneratedDriftTests(unittest.TestCase):
         self.assertEqual(
             (
                 "[Unreleased]",
+                "[0.6.0] - 2026-08-22",
                 "[0.5.0] - 2026-07-13",
                 "[0.4.5] - 2026-05-18",
                 "[0.4.2] - 2026-04-23",
